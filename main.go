33de9ee735c90f0b2f0939db35ca4e484c10e4c0
Yangsibai <yangsibai@gmail.com>
2015-08-27 10:13:11 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+// https://developers.google.com/web-search/docs/reference#_intro_fonje
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&hl=zh&q=node.js")
+}
