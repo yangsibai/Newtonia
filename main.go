0c2d12a6e675ecff57ab2380f3c36da392c8b295
Yangsibai <yangsibai@gmail.com>
2015-08-27 18:03:34 +0800

    request api

--- main.go
+++ main.go
@@ -3,9 +3,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&hl=zh&q=node.js")
+	err, result := search("node")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(result)
+}
+
+type Result struct {
+	responseData struct {
+		results []struct {
+			GsearchResultClass string
+			unescapedUrl       string
+			url                string
+			visibleUrl         string
+			cacheUrl           string
+			title              string
+			titleNoFormatting  string
+			content            string
+		}
+		cursor struct {
+			resultCount string
+			pages       []struct {
+				start string
+				label uint16
+			}
+			estimatedResultCount string
+			currentPageIndex     uint16
+			moreResultsUrl       string
+			searchResultTime     string
+		}
+	}
+	responseStatus uint8
+}
+
+func search(word string) (error, Result) {
+	var result Result
+	url := "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&hl=zh&q=" + word
+	fmt.Println(url)
+	res, err := http.Get(url)
+	if err != nil {
+		return err, result
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close() // must close the body when finished with it
+	if err != nil {
+		return err, result
+	}
+	fmt.Println(body)
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return err, result
+	}
+	return nil, result
 }
