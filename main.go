4df7ca83e33695326200227b9f9e4a8b9dee88bd
Yangsibai <yangsibai@gmail.com>
2015-08-29 20:49:10 +0800

    delete commented code

--- main.go
+++ main.go
@@ -8,22 +8,6 @@ import (
 )
 
 func main() {
-	/*
-	 *err, result := search("node")
-	 *if err != nil {
-	 *log.Fatal(err)
-	 *fmt.Println(err)
-	 *return
-	 *}
-	 *fmt.Println(result)
-	 */
-	/*
-	 *err, result := GoogleSearch("node")
-	 *if err != nil {
-	 *    log.Fatal(err)
-	 *}
-	 *fmt.Println(result)
-	 */
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
